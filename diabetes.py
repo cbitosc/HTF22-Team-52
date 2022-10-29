fc758e19a3f8c72c162064de01b08a0c183c2754
PedamalluAbhiram <ugs207145_it.abhiram@cbit.org.in>
2022-10-29 23:59:43 +0530

    Update diabetes.py

--- diabetes.py
+++ diabetes.py
@@ -34,13 +34,14 @@ input_data = (5,166,72,19,175,25.8,0.587,51)
 # changing the input_data to numpy array
 input_data_as_numpy_array = np.asarray(input_data)
 
-# reshape the array as we are predicting for one instance
+#reshape the array as we are predicting for one instance
 input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
 
-prediction = classifier.predict(input_data_reshaped)
+prediction = classifier.predict(input_data)
 print(prediction)
 
 if (prediction[0] == 0):
   print('The person is not diabetic')
 else:
   print('The person is diabetic')
+
