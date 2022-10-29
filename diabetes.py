983f2d1a037fc2455812c2a6f8a5bbb8b29e1ded
PedamalluAbhiram <ugs207145_it.abhiram@cbit.org.in>
2022-10-29 23:09:08 +0530

    Update diabetes.py

--- diabetes.py
+++ diabetes.py
@@ -4,7 +4,7 @@ from sklearn.model_selection import train_test_split
 from sklearn import svm
 from sklearn.metrics import accuracy_score
 diabetes_dataset = pd.read_csv(r'C:\Users\pedam\Downloads\heart.csv')
-print(diabetes_dataset.describe())
+diabetes_dataset.describe()
 diabetes_dataset['target'].value_counts()
 diabetes_dataset.groupby('target').mean()
 # separating the data and labels
@@ -13,3 +13,34 @@ Y = diabetes_dataset['target']
 X_train, X_test, Y_train, Y_test = train_test_split(X,Y, test_size = 0.2, stratify=Y, random_state=2)
 classifier = svm.SVC(kernel='linear')
 classifier.fit(X_train, Y_train)
+#Printing accuracy
+X_train_prediction = classifier.predict(X_train)
+training_data_accuracy = accuracy_score(X_train_prediction, Y_train)
+print('Accuracy score of the training data : ', training_data_accuracy)
+X_test_prediction = classifier.predict(X_test)
+test_data_accuracy = accuracy_score(X_test_prediction, Y_test)
+print('Accuracy score of the test data : ', test_data_accuracy)
+
+
+
+
+
+
+
+##############################
+#Making a prediction system
+input_data = (5,166,72,19,175,25.8,0.587,51)
+
+# changing the input_data to numpy array
+input_data_as_numpy_array = np.asarray(input_data)
+
+# reshape the array as we are predicting for one instance
+input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
+
+prediction = classifier.predict(input_data_reshaped)
+print(prediction)
+
+if (prediction[0] == 0):
+  print('The person is not diabetic')
+else:
+  print('The person is diabetic')
