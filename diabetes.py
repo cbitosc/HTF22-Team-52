764946a2d360ee0f14215c525d59c8103cc108d8
PedamalluAbhiram <ugs207145_it.abhiram@cbit.org.in>
2022-10-29 22:45:27 +0530

    Update diabetes.py

--- diabetes.py
+++ diabetes.py
@@ -3,4 +3,5 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn import svm
 from sklearn.metrics import accuracy_score
-diabetes_dataset = pd.read_csv('') 
+diabetes_dataset = pd.read_csv('C:\Users\pedam\Downloads\heart.csv')
+diabetes_dataset.head() 
