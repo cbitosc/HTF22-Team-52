6abf16a11d15efa46933f29a32d36894028b9abb
PedamalluAbhiram <ugs207145_it.abhiram@cbit.org.in>
2022-10-29 22:33:03 +0530

    Update diabetes.py

--- diabetes.py
+++ diabetes.py
@@ -1,2 +1,5 @@
-import pandas as pd
 import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn import svm
+from sklearn.metrics import accuracy_score
